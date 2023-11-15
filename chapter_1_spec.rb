66294a82c32adc9eef401d0f2b0bdfa3a718d1fe
Ashik Varghese <ashikv@nulogy.com>
2023-11-15 15:48:53 -0500

    Add spec for Customer#statement

---
+++ chapter_1_spec.rb
@@ -0,0 +1,28 @@
+require_relative 'chapter_1'
+require 'active_support/all'
+
+describe "Customer" do
+  it "generates a statement" do
+    customer = Customer.new('Ashik')
+    customer.add_rental(build_rental('Dune 2', Movie::NEW_RELEASE, days_rented: 1))
+    customer.add_rental(build_rental("Chinatown", Movie::REGULAR, days_rented: 2))
+    customer.add_rental(build_rental("Toy Story", Movie::CHILDRENS, days_rented: 3))
+    expected_statement = <<~STATEMENT
+      Rental Record for Ashik
+         Dune 2  3
+         Chinatown 2
+         Toy Story 1.5
+       Amount owed is 6.5
+       You earned 3 frequent renter points
+    STATEMENT
+
+    statement = customer.statement
+
+    expect(statement.squish).to eq(expected_statement.squish), statement
+  end
+
+  def build_rental(title, price_code, days_rented:)
+    movie = Movie.new(title, price_code)
+    Rental.new(movie, days_rented)
+  end
+end
