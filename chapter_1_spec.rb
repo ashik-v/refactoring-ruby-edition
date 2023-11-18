216232df3f2eeb047c0136fb2d5c35bf75c0bc03
Ashik Varghese <ashikv@nulogy.com>
2023-11-18 11:41:17 -0500

    Complete spec coverage

--- chapter_1_spec.rb
+++ chapter_1_spec.rb
@@ -21,6 +21,51 @@ describe "Customer" do
     expect(statement.squish).to eq(expected_statement.squish), statement
   end
 
+  it "generates a statement for a regular movie greater than 2 days" do
+    customer = Customer.new('Ashik')
+    customer.add_rental(build_rental("Chinatown", Movie::REGULAR, days_rented: 3))
+    expected_statement = <<~STATEMENT
+      Rental Record for Ashik
+         Chinatown 3.5
+       Amount owed is 3.5
+       You earned 1 frequent renter points
+    STATEMENT
+
+    statement = customer.statement
+
+    expect(statement.squish).to eq(expected_statement.squish), statement
+  end
+
+  it "generates a statement for a childrens movie greater than 2 days" do
+    customer = Customer.new('Ashik')
+    customer.add_rental(build_rental("Toy Story", Movie::CHILDRENS, days_rented: 4))
+    expected_statement = <<~STATEMENT
+      Rental Record for Ashik
+         Toy Story 3.0
+       Amount owed is 3.0
+       You earned 1 frequent renter points
+    STATEMENT
+
+    statement = customer.statement
+
+    expect(statement.squish).to eq(expected_statement.squish), statement
+  end
+
+  it "gives a bonus for a new release rental of two days or more" do
+    customer = Customer.new('Ashik')
+    customer.add_rental(build_rental("Dune 2", Movie::NEW_RELEASE, days_rented: 3))
+    expected_statement = <<~STATEMENT
+      Rental Record for Ashik
+         Dune 2 9
+       Amount owed is 9
+       You earned 2 frequent renter points
+    STATEMENT
+
+    statement = customer.statement
+
+    expect(statement.squish).to eq(expected_statement.squish), statement
+  end
+
   def build_rental(title, price_code, days_rented:)
     movie = Movie.new(title, price_code)
     Rental.new(movie, days_rented)
