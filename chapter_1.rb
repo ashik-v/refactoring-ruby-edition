0c9c30160cf0059fd8643fcfeb752267ee0a4057
Ashik Varghese <ashikv@nulogy.com>
2023-11-26 09:32:00 -0500

    Refactor: Extract method

--- chapter_1.rb
+++ chapter_1.rb
@@ -37,11 +37,7 @@ class Customer
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
       # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
-        frequent_renter_points += 1
-      end
+      frequent_renter_points += frequent_renter_points_for(element)
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + rental_amount(element).to_s + "\n"
       total_amount += rental_amount(element)
@@ -50,6 +46,17 @@ class Customer
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
+  end
+
+  def frequent_renter_points_for(rental)
+    frequent_renter_points = 0
+    frequent_renter_points += 1
+    # add bonus for a two day new release rental
+    if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
+      return frequent_renter_points + 1
+    end
+
+    frequent_renter_points
   end
 
   def rental_amount(rental)
