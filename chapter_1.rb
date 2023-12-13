bf57f0bcf7e6a1613463574cb9626fcee0a52de2
Ashik Varghese <ashikv@nulogy.com>
2023-12-13 18:45:40 -0500

    try_2-: Extract Rental#amount

--- chapter_1.rb
+++ chapter_1.rb
@@ -17,6 +17,22 @@ class Rental
 	def initialize(movie, days_rented)
 		@movie, @days_rented = movie, days_rented
 	end
+
+	def amount
+		this_amount = 0
+		case movie.price_code
+		when Movie::REGULAR
+			this_amount += 2
+			this_amount += (days_rented - 2) * 1.5 if days_rented > 2
+		when Movie::NEW_RELEASE
+			this_amount += days_rented * 3
+		when Movie::CHILDRENS
+			this_amount += 1.5
+			this_amount += (days_rented - 3) * 1.5 if days_rented > 3
+		end
+
+		this_amount
+	end
 end
 
 
@@ -36,20 +52,6 @@ class Customer
 		total_amount, frequent_renter_points = 0, 0
 		result = "Rental Record for #{@name}\n"
 		@rentals.each do |rental|
-			this_amount = 0
-			
-			# determine amounts for each line
-			case rental.movie.price_code
-			when Movie::REGULAR
-				this_amount += 2
-				this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-			when Movie::NEW_RELEASE
-				this_amount += rental.days_rented * 3
-			when Movie::CHILDRENS
-				this_amount += 1.5
-				this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-			end
-			
 			# add frequent renter points
 			frequent_renter_points += 1
 			# add bonus for a two day new release rental
@@ -57,8 +59,8 @@ class Customer
 				frequent_renter_points += 1
 			end
 			# show figures for this rental
-			result += "\t" + rental.movie.title + "\t" + this_amount.to_s + "\n"
-			total_amount += this_amount
+			result += "\t" + rental.movie.title + "\t" + rental.amount.to_s + "\n"
+			total_amount += rental.amount
 		end
 		#add footer lines
 		result += "Amount owed is #{total_amount}\n"
