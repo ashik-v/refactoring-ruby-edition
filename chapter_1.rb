f5feb901063f263c56e486a936826e37ccfa3448
Ashik Varghese <ashikv@nulogy.com>
2023-12-13 18:56:02 -0500

    try_2-: Simplify

--- chapter_1.rb
+++ chapter_1.rb
@@ -35,12 +35,12 @@ class Rental
 	end
 
 	def frequent_renter_points
-		frequent_renter_points = 1
 		# add bonus for a two day new release rental
 		if movie.price_code == Movie::NEW_RELEASE && days_rented > 1
-			frequent_renter_points += 1
+			2
+		else
+			1
 		end
-		frequent_renter_points
 	end
 end
 
